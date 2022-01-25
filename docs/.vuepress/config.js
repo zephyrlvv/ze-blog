d0585823a93943db66e9132a322c1b63711746c9
zephyr lvv <82691774+zephyrlvv@users.noreply.github.com>
2022-01-25 16:38:56 +0800

    临时

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,14 +3,19 @@ module.exports = {
   lang: 'zh-CN',
   title: 'ze-blog',
   description: 'this is my personal blog',
+  base:'/docs/',
   // 主题和它的配置
   theme: '@vuepress/theme-default',
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
     navbar:[
       {
+        text:'首页',
+        link:'/',
+      },
+      {
         text:'Group',
-        link:'/fool/'
+       link:'/group/',
       }
     ]
   },
